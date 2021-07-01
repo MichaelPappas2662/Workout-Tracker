513865987884508f8d6a0e4527a46c80dcaa4bb7
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-01 15:53:09 +1000

    exporting the model from a single file

--- models/Workout.js
+++ models/Workout.js
@@ -2,45 +2,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now
+const WorkoutSchema = new Schema(
+  {
+    day: { type: Date, default: () => new Date() },
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        duration: {
+          type: Number,
+        },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number,
+      },
+    ],
   },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-      },
-      name: {
-        type: String,
-        trim: true,
-      },
-      duration: Number,
-      weight: {
-        type: Number,
-        default: 0
-      },
-      reps: {
-        type: Number,
-        default: 0
-      },
-      sets: {
-        type: Number,
-        default: 0
-      },
-      distance: {
-        type: Number,
-        default: 0
-      }
-    }
-  ],
-  totalDuration: {
-    type: Number,
-    default: 0,
+  {
+    toJSON: {
+      virtuals: true,
+    },
   }
+);
 
+WorkoutSchema.virtual("totalDuration").get(function () {
+  const duration = this.exercises.reduce((acc, cur) => {
+    return acc + cur.duration;
+  }, 0);
+
+  return duration;
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
