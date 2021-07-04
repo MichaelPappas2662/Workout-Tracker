a87d4937eb15defc053416db0a4971063540a201
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-04 14:11:59 +1000

    debugigng the workout file

--- models/Workout.js
+++ models/Workout.js
@@ -1,43 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
-  {
-    day: { type: Date, default: () => new Date() },
-    exercises: [
-      {
-        type: {
-          type: String,
-        },
-        name: {
-          type: String,
-        },
-        duration: {
-          type: Number,
-        },
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number,
-      },
-    ],
-  },
-  {
-    toJSON: {
-      virtuals: true,
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
     },
-  }
-);
-
-WorkoutSchema.virtual("totalDuration").get(function () {
-  const duration = this.exercises.reduce((acc, cur) => {
-    return acc + cur.duration;
-  }, 0);
-
-  return duration;
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true,
+                minLength: 1,
+            },
+            duration: {
+                type: Number,
+                min: 1,
+            },
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number,
+        },
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workouts", WorkoutSchema);
 
 module.exports = Workout;
